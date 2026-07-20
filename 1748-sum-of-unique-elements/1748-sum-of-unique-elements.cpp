fac5e2fb5e40d2574c3a25364c88d0776088ebcf
Nishant Singh Yadav <263435187+NishantSyadav@users.noreply.github.com>
2026-07-20 21:08:22 +0530

    Time: 3 ms (9.71%), Space: 10.8 MB (45.29%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        
+        unordered_map<int,int>m;
+        int sum = 0;
+
+        for(int i : nums){
+            m[i]++;
+        }
+
+        for(auto it: m){
+            if(it.second == 1){
+                sum += it.first;
+            }
+        }
+        return sum;
+    }
+};
